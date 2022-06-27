6176320845293884dec8c1b6dc5e68908b294434
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2022-06-27 16:31:36 +0800

    Create install_environment.cmd

---
+++ install_environment.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+echo Press Any Key To Start Environment Installation...
+pause
+
+pip install pyserial
+pip install telepot
+pip install phone
+pip install configparser
+pip install idna
+
+pip3 install pyserial
+pip3 install telepot
+pip3 install phone
+pip3 install configparser
+pip3 install idna
+
+echo Finish!
+pause
+exit
